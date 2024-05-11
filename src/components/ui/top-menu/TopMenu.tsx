1ad9341e8c9632c89bda8bf8c6f7dcf99ec37b40
k4cdev <kdoliveros@indracompany.com>
2024-05-11 17:05:57 -0500

    Creating sidebar animation, added zustand and clsx libs

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -1,10 +1,15 @@
+'use client';
+
 import { titleFont } from '@/config/font'
 import { IoSearchOutline, IoCartOutline, IoStatsChartOutline } from 'react-icons/io5';
 
 import Link from 'next/link'
 import React from 'react'
+import { useUIStore } from '@/store';
 
 export const TopMenu = () => {
+    const openSideMenu = useUIStore( state => state.openSideMenu );
+
     return (
         <nav className="flex px-5  justify-between items-center w-full">
 
@@ -37,7 +42,7 @@ export const TopMenu = () => {
                     </div>
                 </Link>
 
-                <button className="m-2 p-2 rounded-md transition-all hover:bg-gray-500">
+                <button  onClick={ openSideMenu } className="m-2 p-2 rounded-md transition-all hover:bg-gray-100">
                     menú
                 </button>
             </div>
